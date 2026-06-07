c9a7b003ecd11c55f0a1d5ce8b5b2ab2ce82bf95
mohebimohammad <muhammadmohebi123@gmail.com>
2026-06-07 05:36:03 +0330

    enviroment mangment

--- core/database.py
+++ core/database.py
@@ -3,9 +3,10 @@ from fastapi import status, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import sessionmaker, declarative_base 
 from datetime import datetime
+from config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL,
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL,
                        connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
